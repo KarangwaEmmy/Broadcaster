531d1a670204dcf1c04b8833d9c0d4c98c84fa8a
Emmy Karangwa <kemmyxy@gmail.com>
2019-11-25 11:24:58 +0200

    API

---
+++ Server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+import userRouter from './routes/userRouter';
+import flagRoute from './routes/flagRoute'
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send({
+  status:(200),
+  message: ' Welcome to Broadcaster API endpoints!'
+  + 'The platform of reg flags and intervations'
+}));
+
+app.use('/api/v1/auth', userRouter);
+app.use('/api/v1', flagRoute);
+
+// eslint-disable-next-line no-undef
+let port = process.env.PORT;
+if (port == null || port === '') {
+  port = 7000;
+}
+
+const server = app.listen(port, console.log(`App running on port ${port}`));
+
+export default server;
