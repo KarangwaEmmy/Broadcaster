cadadac67fa207f93156df57950cd6b51809d38e
Emmy Karangwa <kemmyxy@gmail.com>
2019-11-27 16:11:10 +0200

    bug(UI): UI feedbacks [finishes #169999308]

--- Server/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import  User from '../controller/userController';
-
-const router = Router();
-router.post('/signup',  User.create);
- router.get('/users', User.fetchAll);
- router.post('/login', User.Login);
-
-export default router;
