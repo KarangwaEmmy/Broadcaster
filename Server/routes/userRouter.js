3d34987b878cc1ddfc3e1e1dd0eaae8db01f2bdc
Emmy Karangwa <kemmyxy@gmail.com>
2019-11-28 11:07:26 +0200

    routes

---
+++ Server/routes/userRouter.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import  User from '../controller/userController';
+import {SignupValidator, loginValidator} from '../Middleware/Validation'
+
+const router = Router();
+router.post('/signup', SignupValidator, User.create);
+ router.post('/login',loginValidator, User.Login);
+
+export default router;
