e185fbd07890a72efb1d0d321e8c2db5932282cf
Emmy Karangwa <kemmyxy@gmail.com>
2019-11-29 11:06:23 +0200

    refactor

--- Server/Middleware/Validation.js
+++ Server/Middleware/Validation.js
@@ -1,40 +1,27 @@
 import Joi from 'joi'
 import { serverResponse } from '../Helper/Response'
 
-const userSchema = Joi.object().keys({
-    firstname: Joi.string().required(),
-    lastname: Joi.string().min(3).max(12).required(),
-    email: Joi.string().min(3).max(255).required(),
-    phoneNumber: Joi.number().integer().min(10).required(),
-    username: Joi.string().min(3).max(12).required(),
-    password: Joi.string().min(4).regex(/^[a-zA-Z0-9]{3,30}$/).required().trim()
-});
 
 
-const loginSchema = Joi.object().keys({
-    email: Joi.string().min(3).max(255).required(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().trim()
-});
+class Validation{
+  
 
-const UpdateLocationSchema = Joi.object().keys({
-  location: Joi.string().min(3).max(25).required()
-})
 
-const UpdateCommentSchema = Joi.object().keys({
-  comment: Joi.string().min(10).max(255).required()
-})
-const incidentSchema = Joi.object().keys({
-    title: Joi.string().min(3).max(255).trim(),
-    type: Joi.string().min(3).max(255).label('red flag', 'intervention').trim(),
-    location: Joi.string().min(3).max(255).required(),
-    status: Joi.string().label('draft', 'under investigation', 'resolved', 'rejected').trim(),
-    comment: Joi.string().min(3).max(255)
-})
-const errorMessage = (err, res) => {
+
+
+
+
+
+
+ errorMessage = (err, res) => {
   const errMessage = err.details[0].message;
   return serverResponse(res, 422, ...['status', 'error', 'Message', errMessage]);
 };
-const loginValidator = (req, res, next) => {
+ loginValidator = (req, res, next) => {
+  const loginSchema = Joi.object().keys({
+    email: Joi.string().min(3).max(255).required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().trim()
+});
     return Joi.validate(req.body, loginSchema, (err, value) => {
       if (err) {
         return errorMessage(err, res);
@@ -43,7 +30,15 @@ const loginValidator = (req, res, next) => {
     });
   };
 
-const SignupValidator = (req, res, next) => {
+ SignupValidator = (req, res, next) => {
+  const userSchema = Joi.object().keys({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().min(3).max(12).required(),
+    email: Joi.string().min(3).max(255).required(),
+    phoneNumber: Joi.number().integer().min(10).required(),
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(4).regex(/^[a-zA-Z0-9]{3,30}$/).required().trim()
+});
   return Joi.validate(req.body, userSchema, (err, value) => {
     if(err){
       return errorMessage(err, res)
@@ -52,7 +47,16 @@ const SignupValidator = (req, res, next) => {
   })
 }
 
-const IncidentValidator = (req, res, next) => {
+ IncidentValidator = (req, res, next) => {
+
+  const incidentSchema = Joi.object().keys({
+    title: Joi.string().min(3).max(255).trim(),
+    type: Joi.string().min(3).max(255).label('red flag', 'intervention').trim(),
+    location: Joi.string().min(3).max(255).required(),
+    status: Joi.string().label('draft', 'under investigation', 'resolved', 'rejected').trim(),
+    comment: Joi.string().min(3).max(255)
+})
+
   let { title, type, comment} = req.body;
   return Joi.validate(req.body, incidentSchema, (error, value) =>{
     if(error){
@@ -61,7 +65,12 @@ const IncidentValidator = (req, res, next) => {
     return next();
   })
 }
-const LocationValidation = (req, res, next) =>{
+ LocationValidation = (req, res, next) =>{
+
+  const UpdateLocationSchema = Joi.object().keys({
+    location: Joi.string().min(3).max(25).required()
+  })
+
   return Joi.validate(req.body, UpdateLocationSchema, (err, value) => {
     if(err){
       return errorMessage(err, res);
@@ -70,7 +79,12 @@ const LocationValidation = (req, res, next) =>{
   });
 }
 
-const CommentValidation = (req, res, next) => {
+ CommentValidation = (req, res, next) => {
+
+  const UpdateCommentSchema = Joi.object().keys({
+    comment: Joi.string().min(10).max(255).required()
+  })
+
   return Joi.validate(req.body, UpdateCommentSchema, (err, value) => {
     if(err){
       return errorMessage(err, res);
@@ -78,4 +92,6 @@ const CommentValidation = (req, res, next) => {
     return next();
   })
 }
-export {loginValidator, SignupValidator, IncidentValidator,LocationValidation, CommentValidation}
+}
+
+export default  Validation();
--- Server/routes/flagRoute.js
+++ Server/routes/flagRoute.js
@@ -1,20 +1,21 @@
 import { Router } from 'express';
-import { IncidentValidator,LocationValidation, CommentValidation} from '../middleware/Validation';
+import { Validation} from '../middleware/Validation';
 import {checkToken} from '../Middleware/Auth'
 import  {postFlag, fetchAllFlags, getOneFlag, deleteFlag, updateLocation, UpdateComment} from '../controller/flagController';
 
+const { IncidentValidator,LocationValidation, CommentValidation} = Validation;
 const router = Router();
-router.post('/red-flags',IncidentValidator,  postFlag);
+router.post('/red-flags',checkToken,IncidentValidator,  postFlag);
 
-router.get('/red-flags', fetchAllFlags);
+router.get('/red-flags',checkToken, fetchAllFlags);
 
-router.get('/red-flags/:id',  getOneFlag);
+router.get('/red-flags/:id', checkToken, getOneFlag);
 
-router.patch('/red-flags/:id/location', LocationValidation,  updateLocation);
+router.patch('/red-flags/:id/location',checkToken, LocationValidation,  updateLocation);
 
-router.patch('/red-flags/:id/comment', CommentValidation,  UpdateComment);
+router.patch('/red-flags/:id/comment',checkToken, CommentValidation,  UpdateComment);
 
-router.delete('/red-flags/:id',  deleteFlag);
+router.delete('/red-flags/:id',checkToken, deleteFlag);
 
 
 
