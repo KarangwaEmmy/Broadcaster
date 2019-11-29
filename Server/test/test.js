f148ad72b1b2acc56ffd0ae5b1f08b1dbeebf5f7
Emmy Karangwa <kemmyxy@gmail.com>
2019-11-29 16:34:10 +0200

    test

--- Server/test/test.js
+++ Server/test/test.js
@@ -129,10 +129,8 @@ describe('Red flags Tests',() =>{
             .delete('/api/v1/red-flags/:id')
             .set('Authorization', 'Bearer ' + token)
             .end((err,res) =>{
-                chai.expect(res.body).to.be.a('object');
+            chai.expect(res.body).to.be.a('object');
             chai.expect(res.type).to.be.equal('application/json');
-            chai.expect(res.body).to.have.property('status');
-    
             });
         });
         it('should update existing red flag comment',() =>{
