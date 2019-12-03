63cf5460113a51dc2952a9b08790f5a8d3a71ec4
Emmy Karangwa <kemmyxy@gmail.com>
2019-12-03 11:43:02 +0200

    feature(API): challenge 3 setting up database[finishes #170071221]

---
+++ Server/V2/db/Tables/createTables.js
@@ -0,0 +1,38 @@
+import dbQuery from '../config';
+
+const sqlQueries = {};
+
+const allalltables = `
+               CREATE TABLE IF NOT EXISTS users(
+                  id SERIAL PRIMARY KEY,
+                  email VARCHAR(128) UNIQUE NOT NULL,
+                  firstname VARCHAR(128) NOT NULL,
+                  lastname VARCHAR(128) NOT NULL,
+                  phonenumber VARCHAR(255) UNIQUE NOT NULL,
+                  password VARCHAR(255) NOT NULL,
+                  username VARCHAR(255) UNIQUE NOT NULL,
+                  isadmin VARCHAR(255) NOT NULL
+                  );
+                  
+                  
+                  CREATE TABLE IF NOT EXISTS intervention(
+                    id SERIAL PRIMARY KEY,
+                    created_on VARCHAR NOT NULL,
+                    user_id INTEGER REFERENCES users(id) NOT NULL,
+                    createdBy VARCHAR(128) REFERENCES users(email)  NOT NULL,
+                    title VARCHAR(128) NOT NULL,
+                    type VARCHAR(128) NOT NULL,
+                    location VARCHAR(128) NOT NULL,
+                    status VARCHAR(128) NOT NULL,
+                    image_url VARCHAR(128) NOT NULL,
+                    video_url VARCHAR(128) NOT NULL,
+                    comment VARCHAR(255) NOT NULL, 
+                    );                  
+                    
+`;
+
+
+sqlQueries.alltables = alltables;
+ dbQuery(alltables);
+
+ export default sqlQueries;
